a95cfb0ce58ef8a406c112f5b2e47783f5597c7b
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-05-02 22:57:32 +0700

    feat: Notes constant

--- src/Types/Types.ts
+++ src/Types/Types.ts
@@ -1,4 +1,4 @@
-export enum INote {
+export enum INoteEnum {
 	'A' = 'A',
 	'B' = 'B',
 	'C' = 'C',
@@ -6,4 +6,10 @@ export enum INote {
 	'E' = 'E',
 	'F' = 'F',
 	'G' = 'G',
+}
+
+export interface INote {
+	note: INoteEnum
+	octave: number
+	file: string
 }
