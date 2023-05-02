152c76ef3146333976fe727c5656baac602d5c83
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-05-02 21:11:06 +0700

    feat: Background

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import { FC } from 'react'
 
+import { Container, AttributionContainer } from './AppStyles'
+
 const App: FC = () => {
-	return <div></div>
+	return (
+		<Container>
+			<AttributionContainer>
+				Background by{' '}
+				<a href='https://www.freepik.com/free-vector/flat-design-wood-texture-illustration_22628444.htm#query=cartoon%20wood%20texture&position=2&from_view=keyword&track=ais#position=2&query=cartoon%20wood%20texture'>
+					Freepik
+				</a>
+			</AttributionContainer>
+		</Container>
+	)
 }
 
 export default App
---
+++ src/AppStyles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+	width: 100%;
+	height: 100vh;
+
+	background-image: url('/WoodBackground.jpg');
+	background-repeat: no-repeat;
+	background-size: cover;
+
+	position: relative;
+`
+
+export const AttributionContainer = styled.div`
+	position: absolute;
+	bottom: 10px;
+	right: 10px;
+
+	color: #fff;
+
+	& a {
+		color: #fff;
+	}
+`
